b4754209f85c71eafe96bffacaa7d85f5aff625c
ohiduzzamansiam <ohiduzzamansiam@gmail.com>
2022-10-09 07:58:40 +0600

    Changing to geoLocation

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
-import sky from "../assets/sky.png";
 import "./Home.css";
+import axios from "axios";
 
 const fr_cl = (fr) => {
   return (((fr - 32) * 5) / 9).toFixed(0);
@@ -12,25 +12,33 @@ const flu = (str) => {
 const Home = () => {
   const [data, setData] = useState([]);
   const [isLoading, setIsloading] = useState(true);
+  const [country, setCountry] = useState([]);
   useEffect(() => {
-    const getData = async () => {
-      await fetch(
-        "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/bangladesh?key=GK5PKAP745HMQ8YVZXWDWB62L"
-      )
-        .then((res) => res.json())
-        .then((temps) => {
-          setData(temps);
-          setIsloading(false);
-        });
+    const getCountry = async () => {
+      let res = await axios.get("https://ipinfo.io/json?token=4f66e17bb00ee4");
+      let data = await res.data;
+      setCountry(data.city);
     };
-    getData();
-    console.log(data);
+    getCountry();
   }, []);
+  useEffect(() => {
+    if (country.length) {
+      const getTemp = async () => {
+        let res = await axios.get(
+          `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${country}?key=GK5PKAP745HMQ8YVZXWDWB62L`
+        );
+        let data = await res.data;
+        setData(data);
+        setIsloading(false);
+      };
+      getTemp();
+    }
+  }, [country]);
 
   if (isLoading === false) {
     return (
       <>
-        <div className="main_container">
+        <div className="main_container lg:max-w-[30%] lg:mx-auto">
           <div className="blue_section min-h-[70vh] bg-blue-600 shadow-lg shadow-gray-400 flex flex-col justify-center space-y-10 text-center py-5 rounded-b-[50px]">
             <div className="country">
               <h1 className="text-3xl font-bold text-white">
@@ -49,7 +57,7 @@ const Home = () => {
             </div>
           </div>
 
-          <div className="yellow_section bg-amber-400 mx-3 mt-10 rounded-2xl">
+          <div className="yellow_section shadow-lg shadow-gray-400 bg-amber-400 mx-3 mt-10 rounded-2xl">
             <div className="temps flex justify-evenly text-center py-7">
               <div className="min_temps flex flex-col">
                 <span className="text-xl font-extrabold text-white">
